3079c55efcc9f70b50ee9069d912f490a30eb587
all-anime01 <102379035+all-anime01@users.noreply.github.com>
2022-04-10 02:07:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,4 +37,22 @@ $(".input-search").keyup(function(){
     }
 });
 
+//Reporte Episodio
 
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelectorAll(".cta")[0];
+let modal = document.querySelectorAll(".modal")[0];
+let modalC = document.querySelectorAll(".modal-container")[0];
+
+abrir.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modal-close");
+    modalC.style.opacity = "0";
+    modalC.style.visibility = "hidden";
+})
