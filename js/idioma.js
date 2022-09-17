06753bcf2922ea8c654e9336c765934ef3504edf
all-anime01 <102379035+all-anime01@users.noreply.github.com>
2022-09-17 01:56:40 -0500

    Add files via upload

---
+++ js/idioma.js
@@ -0,0 +1,17 @@
+//Cambio de idioma
+
+$('ul.tabs li a:first').addClass('active');
+    $('.secciones article').hide();
+    $('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+        $('.secciones article').hide();
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+
+
--- js/script.js
+++ js/script.js
@@ -37,20 +37,6 @@ $(".input-search").keyup(function(){
     }
 });
 
-//Filtro de reproductor
-
-const targets = document.querySelectorAll('[data-target]')
-const content = document.querySelectorAll('[data-content]')
-
-targets.forEach(target => {
-	target.addEventListener('click', () => {
-		content.forEach(c => {
-			c.classList.remove('active')
-		})
-		const t = document.querySelector(target.dataset.target)
-		t.classList.add('active')
-	})
-})
 
 //Reporte Episodio
 
