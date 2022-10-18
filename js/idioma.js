fa6d6e1dd8650c17f0e5055f05bb6fbef16d0253
bryan101719 <bryan101719@gmail.com>
2022-10-18 18:38:18 -0500

    update

--- js/idioma.js
+++ js/idioma.js
@@ -15,49 +15,49 @@ $('ul.tabs li a:first').addClass('active');
     });
 
 
-//indexof
-const listados = [
-  {nombre: 'Bleach', valor: 500},
-  {nombre: 'Naruto', valor: 500},
-  {nombre: 'Naruto Shippuden', valor: 500},
-  {nombre: 'Kimetsu no Yaiba', valor: 500},
-  {nombre: 'Shingeki no Kyojin', valor: 500},
-  {nombre: 'Ousama Ranking', valor: 500},
-  {nombre: 'Jujutsu Kaisen', valor: 500},
-  {nombre: 'Shaman King (2021)', valor: 500},
-  {nombre: 'Saint Seiya Lost Canvas', valor: 500},
-  {nombre: 'Death note', valor: 500},
-  {nombre: 'One piece', valor: 500},
-  {nombre: 'Boku no Hero Academy', valor: 500},
-  {nombre: 'Chainsaw man', valor: 500},
-]
-const formulario = document.querySelector('#mysearch');
-const btnBusqueda = document.querySelector('#btn-busqueda')
-const resultado = document.querySelector('#resultado');
+// //indexof
+// const listados = [
+//   {nombre: 'Bleach', valor: 500},
+//   {nombre: 'Naruto', valor: 500},
+//   {nombre: 'Naruto Shippuden', valor: 500},
+//   {nombre: 'Kimetsu no Yaiba', valor: 500},
+//   {nombre: 'Shingeki no Kyojin', valor: 500},
+//   {nombre: 'Ousama Ranking', valor: 500},
+//   {nombre: 'Jujutsu Kaisen', valor: 500},
+//   {nombre: 'Shaman King (2021)', valor: 500},
+//   {nombre: 'Saint Seiya Lost Canvas', valor: 500},
+//   {nombre: 'Death note', valor: 500},
+//   {nombre: 'One piece', valor: 500},
+//   {nombre: 'Boku no Hero Academy', valor: 500},
+//   {nombre: 'Chainsaw man', valor: 500},
+// ]
+// const formulario = document.querySelector('#mysearch');
+// const btnBusqueda = document.querySelector('#btn-busqueda')
+// const resultado = document.querySelector('#resultado');
 
-const filtrar = ()=>{
-  // console.log(formulario.value);
-  resultado.innerHTML = '';
-  const texto = formulario.value.toLowerCase();
-  for(let listado of listados){
-      let nombre = listado.nombre.toLowerCase();
-      if(nombre.indexOf !== -1){
-          resultado.innerHTML += `
-          <h3>${listados.nombre} - Valor: ${listados.valor}</h3>
-          `
-      }
-  }
+// const filtrar = ()=>{
+//   // console.log(formulario.value);
+//   resultado.innerHTML = '';
+//   const texto = formulario.value.toLowerCase();
+//   for(let listado of listados){
+//       let nombre = listado.nombre.toLowerCase();
+//       if(nombre.indexOf !== -1){
+//           resultado.innerHTML += `
+//           <h3>${listados.nombre} - Valor: ${listados.valor}</h3>
+//           `
+//       }
+//   }
 
-  if(resultado.innerHTML === ''){
-      resultado.innerHTML += `
-          <h3>no se encontró el resultado...</h3>
-      `
-  }
-}
-btnBusqueda.addEventListener('click', filtrar)
-formulario.addEventListener('Keyup', filtrar)
+//   if(resultado.innerHTML === ''){
+//       resultado.innerHTML += `
+//           <h3>no se encontró el resultado...</h3>
+//       `
+//   }
+// }
+// btnBusqueda.addEventListener('click', filtrar)
+// formulario.addEventListener('Keyup', filtrar)
 
-filtrar();
+// filtrar();
 
 //Funcion Busqueda tabla dapage2
 
