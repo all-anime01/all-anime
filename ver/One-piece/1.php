adc8136a84966741f9fa5b73eb305dd2564e94eb
all.anime01 <all.anime.lat01@gmail.com>
2022-10-27 17:23:16 -0500

    update a php 1onepiece

---
+++ ver/One-piece/1.php
@@ -0,0 +1,89 @@
+<?php include("C:/xampp/htdocs/all-anime/template/cabecera.php"); ?>
+<title>One piece Episodio 01 Latino — All-Anime</title>
+
+<div class="CpCnA">
+    <span class="lgtbx"></span>
+    <nav class="Brdcrmb fa-home">
+        <a href="all-anime.net.html" title="Inicio All-Anime">INICIO</a>
+        <i class="fa-caret-right"></i>
+        <a href="Ousama-Ranking-tv.html">ONE PIECE</a>
+        <i class="fa-caret-right"></i> <strong>One piece Episodio 01</strong>
+    </nav>
+
+    <div class="CapiTop">
+        <h1 class="Title" title="One piece Episodio 01">One piece Episodio 01</h1>
+        <h2 class="SubTitle">Episodio 1</h2>
+        <div class="lang-op"></div>
+    </div>
+
+    <div class="contenedor">
+        <IFRAME SRC="https://sbfull.com/e/nvcmkek4ir5u.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=840 HEIGHT=460 allowfullscreen></IFRAME>
+    </div>
+</div>
+
+<div class="CpCnB">
+    <div class="CapOptns ClFx show">
+        <div class="CapNv">
+            <a href="one-piece-tv.html" class="CapNvLs fa-th-list"></a>
+            <a href="One-piece-Episodio02.html" class="CapNvNx fa-chevron-right">SIGUIENTE</a>
+        </div>
+        <div class="container-text">
+            <a href="#" class="cta Rprt fa-flag-o"><span>REPORTAR</span><span>Episodio</span></a>
+        </div>
+
+
+        <form action="https://formsubmit.co/all.anime.lat01@gmail.com" method="POST">
+
+            <div class="modal-container">
+                <div class="modal modal-close">
+                    <p class="close">X</p>
+                    <h4>Reportar<i class="fa-solid fa-anchor-circle-exclamation"></i></h4>
+                    <div class="modal-text">
+                        <label for="report-resaon">Motivo:</label>
+                        <select class="form-control" id="report_reason">
+                            <option value="0">No reproduce</option>
+                            <option value="1">Problemas de Audio</option>
+                            <option value="2">No se encuentra el Archivo</option>
+                            <option value="3">No es el Episodio</option>
+                        </select>
+                        <div class="form-group">
+                            <label for="report-comment">Explicanos el problema:</label>
+                            <textarea class="form-control" id="report_comment"></textarea>
+                        </div>
+                        <button onclick='mensaje("fue enviado")' class="popup-btn">Enviar Reporte</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+        <script>
+            function mensaje(enviado) {
+                console.log("El mensaje" + " " + enviado);
+                alert("El mensaje" + " " + enviado + " " + "gracias por reportarlo!");
+            }
+        </script>
+        <!-- Inicio comentarios Disqus -->
+
+        <div id="disqus_thread"></div>
+        <script>
+            /**
+             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+            /*
+            var disqus_config = function () {
+            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+            */
+            (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document,
+                    s = d.createElement('script');
+                s.src = 'https://all-anime-comment.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        </section>
+        </section>
+        <?php include("C:/xampp/htdocs/all-anime/template/pie.php"); ?>
