a75283af4cbc98733a445437bd69c8e94bf9a101
all.anime01 <all.anime.lat01@gmail.com>
2023-04-09 16:01:31 -0500

    actual-domingoMario

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
-const fila = document.querySelector('.contenedor-carousel');
-const peliculas = document.querySelectorAll('.pelicula');
+const fila = document.querySelector(".contenedor-carousel");
+const peliculas = document.querySelectorAll(".pelicula");
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
+const flechaIzquierda = document.getElementById("flecha-izquierda");
+const flechaDerecha = document.getElementById("flecha-derecha");
 
 // ? ----- ----- Event Listener para la flecha derecha. ----- -----
-flechaDerecha.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth;
+flechaDerecha.addEventListener("click", () => {
+  fila.scrollLeft += fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.nextSibling){
-		indicadorActivo.nextSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
+  const indicadorActivo = document.querySelector(".indicadores .activo");
+  if (indicadorActivo.nextSibling) {
+    indicadorActivo.nextSibling.classList.add("activo");
+    indicadorActivo.classList.remove("activo");
+  }
 });
 
 // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-flechaIzquierda.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth;
+flechaIzquierda.addEventListener("click", () => {
+  fila.scrollLeft -= fila.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.previousSibling){
-		indicadorActivo.previousSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
-	}
+  const indicadorActivo = document.querySelector(".indicadores .activo");
+  if (indicadorActivo.previousSibling) {
+    indicadorActivo.previousSibling.classList.add("activo");
+    indicadorActivo.classList.remove("activo");
+  }
 });
 
 // ? ----- ----- Paginacion ----- -----
 const numeroPaginas = Math.ceil(peliculas.length / 5);
-for(let i = 0; i < numeroPaginas; i++){
-	const indicador = document.createElement('button');
+for (let i = 0; i < numeroPaginas; i++) {
+  const indicador = document.createElement("button");
 
-	if(i === 0){
-		indicador.classList.add('activo');
-	}
+  if (i === 0) {
+    indicador.classList.add("activo");
+  }
 
-	document.querySelector('.indicadores').appendChild(indicador);
-	indicador.addEventListener('click', (e) => {
-		fila.scrollLeft = i * fila.offsetWidth;
+  document.querySelector(".indicadores").appendChild(indicador);
+  indicador.addEventListener("click", (e) => {
+    fila.scrollLeft = i * fila.offsetWidth;
 
-		document.querySelector('.indicadores .activo').classList.remove('activo');
-		e.target.classList.add('activo');
-	});
+    document.querySelector(".indicadores .activo").classList.remove("activo");
+    e.target.classList.add("activo");
+  });
 }
 
 // ? ----- ----- Hover ----- -----
 peliculas.forEach((pelicula) => {
-	pelicula.addEventListener('mouseenter', (e) => {
-		const elemento = e.currentTarget;
-		setTimeout(() => {
-			peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
-			elemento.classList.add('hover');
-		}, 300);
-	});
+  pelicula.addEventListener("mouseenter", (e) => {
+    const elemento = e.currentTarget;
+    setTimeout(() => {
+      peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
+      elemento.classList.add("hover");
+    }, 300);
+  });
 });
 
-fila.addEventListener('mouseleave', () => {
-	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+fila.addEventListener("mouseleave", () => {
+  peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
 });
