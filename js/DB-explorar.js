4b7b37c6001ca9a286dfd3f16f78e6e7619c6f61
TheBryan <bryan101719@gmail.com>
2024-11-09 00:15:05 -0500

    DBD T1 5

--- js/DB-explorar.js
+++ js/DB-explorar.js
@@ -700,7 +700,7 @@ const animes = [
       "¡Goku comienza una gran aventura en la nueva serie de Dragon Ball!",
     type: "Subtitulado",
     seasons: 1,
-    episodes: 4,
+    episodes: 5,
   },
   {
     id: 56,
--- js/dist/DB-explorar.dev.js
+++ js/dist/DB-explorar.dev.js
@@ -550,7 +550,7 @@ var animes = [{
   description: "¡Goku comienza una gran aventura en la nueva serie de Dragon Ball!",
   type: "Subtitulado",
   seasons: 1,
-  episodes: 4
+  episodes: 5
 }, {
   id: 56,
   name: "Shangri-La Frontier",
