8e775963fbaed5107eb4620a6b36308b4567b5db
TheBryan <bryan101719@gmail.com>
2024-11-22 17:01:46 -0500

    DBD T1 7 full

--- js/DB-explorar.js
+++ js/DB-explorar.js
@@ -700,7 +700,7 @@ const animes = [
       "¡Goku comienza una gran aventura en la nueva serie de Dragon Ball!",
     type: "Subtitulado",
     seasons: 1,
-    episodes: 6,
+    episodes: 7,
   },
   {
     id: 56,
