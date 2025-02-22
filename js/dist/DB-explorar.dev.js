6eed5c5c3402969d9be3db4b19fe1161b75bddf3
TheBryan <bryan101719@gmail.com>
2025-02-22 16:21:43 -0500

    SL T2 20 BE T3P3 20 PH T1 85

--- js/dist/DB-explorar.dev.js
+++ js/dist/DB-explorar.dev.js
@@ -180,7 +180,7 @@ var animes = [{
   description: "Assiah, el mundo humano, y Gehenna, el mundo demoníaco, rara vez se cruzan. Sin embargo, algunos humanos pueden exorcizar demonios: los exorcistas.",
   type: "Subtitulado",
   seasons: 3,
-  episodes: 68
+  episodes: 69
 }, {
   id: 19,
   name: "Avatar: La leyenda de Aang",
@@ -340,7 +340,7 @@ var animes = [{
   description: "Pocket Mosnters es una historia sobre dos misteriosos personajes de Pokémon, Liko y Roy, que están enredados en el mundo de Pokémon. Sus aventuras y destinos cambiarán a medida que se encuentren con nuevas criaturas y encuentros.",
   type: "Sub | Dob",
   seasons: 1,
-  episodes: 84
+  episodes: 85
 }, {
   id: 35,
   name: "Pokémon: Viajes",
@@ -590,7 +590,7 @@ var animes = [{
   description: "Para dominar sus nuevas habilidades en secreto, Jinwoo debe luchar contra los enemigos más duros de la humanidad para salvar a su madre.",
   type: "Sub | Dob",
   seasons: 2,
-  episodes: 19
+  episodes: 20
 }, {
   id: 60,
   name: "Dr. Stone",
