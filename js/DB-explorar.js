2f807f6e6cf0621c195dd8d8d24e56e357594055
TheBryan <bryan101719@gmail.com>
2025-05-02 16:11:48 -0500

    FF T3 5

--- js/DB-explorar.js
+++ js/DB-explorar.js
@@ -854,7 +854,7 @@ const animes = [
       "Tokio está ardiendo y los ciudadanos sufren misteriosas combustiones espontáneas por toda la ciudad. La responsable de apagar este infierno es la Brigada Especial Antiincendios y Shinra está listo para unirse a su lucha.",
     type: "Sub | Dob",
     seasons: 3,
-    episodes: 52,
+    episodes: 53,
   },
   {
     id: 68,
