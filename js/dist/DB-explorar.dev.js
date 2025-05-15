8747a04ff59ddc03f11ba59ee98016df020fb224
TheBryan <bryan101719@gmail.com>
2025-05-15 16:54:12 -0500

    WB T2 20

--- js/dist/DB-explorar.dev.js
+++ js/dist/DB-explorar.dev.js
@@ -120,7 +120,7 @@ var animes = [{
   description: "Haruka Sakura, un novato en la delincuente Preparatoria Furin, busca abrirse paso luchando. La escuela ahora tiene un grupo protector llamado Wind Breaker. Comienza su leyenda.",
   type: "Sub | Dob",
   seasons: 2,
-  episodes: 18
+  episodes: 20
 }, {
   id: 13,
   name: "Tokyo Revenger",
